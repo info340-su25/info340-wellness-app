fcd66eb8f5ad32be954fb08059646cdcd72a65b8
AlttiK <alttikoskinen@gmail.com>
2025-08-08 22:53:16 -0700

    Added DIaryEntry to List file

--- src/pages/Diary/DiaryEntryList.jsx
+++ src/pages/Diary/DiaryEntryList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import DiaryEntry from "./DiaryEntry";
 
-export default function DiaryEntryList() {
+export default function DiaryEntryList(props) {
 
+    // Add DiaryEntry Props to be sent in
 
     return (
         <section className="entry-section">
             <div className="entry-deck">
-                
+                <DiaryEntry />
             </div>
         </section>
     );
