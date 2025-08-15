222938ef4802996d7824c7becdd28117a0c6112f
AarushSharma12 <as2027@uw.edu>
2025-08-14 18:13:05 -0700

    For readability, moved .map() call out of the return statement in LogMenu.jsx

--- src/pages/Log/LogMenu.jsx
+++ src/pages/Log/LogMenu.jsx
@@ -2,19 +2,19 @@ import Card from "../../components/Card";
 import { LOG_CATEGORIES } from "../../data/sampleData";
 
 export default function LogMenu() {
+  const categoryCards = LOG_CATEGORIES.map((cat) => (
+    <Card key={cat.to} to={cat.to} icon={cat.icon}>
+      {cat.label}
+    </Card>
+  ));
+
   return (
-    <>
+    <div>
       <h1>Log</h1>
       <section id="log-menu">
         <h2>Select a category to log</h2>
-        <div className="log-grid">
-          {LOG_CATEGORIES.map((cat) => (
-            <Card key={cat.to} to={cat.to} icon={cat.icon}>
-              {cat.label}
-            </Card>
-          ))}
-        </div>
+        <div className="log-grid">{categoryCards}</div>
       </section>
-    </>
+    </div>
   );
 }
