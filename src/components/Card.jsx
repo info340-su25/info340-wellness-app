0fa87ed025bee3ecbce1dbfc63474664ef00d2af
AarushSharma12 <as2027@uw.edu>
2025-08-22 03:50:28 -0400

    Card component to set default icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 import { NavLink } from "react-router";
 
-export default function Card({ to, icon, children }) {
+export default function Card({ to, icon = "info", children }) {
   return (
     <NavLink to={to} className="log-card">
       <span className="material-symbols-outlined" aria-hidden>
-        hotel
-      </span>{" "}
-      {children}
+        {icon}
+      </span>
+      <span className="card-label">{children}</span>
     </NavLink>
   );
 }
